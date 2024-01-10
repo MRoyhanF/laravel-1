d9e89d1dd990e236e46688c38b30d6580055c8e8
Muhamad Royhan Fadhli <122357726+MRoyhanF@users.noreply.github.com>
2024-01-10 15:12:12 +0700

    fix the ui

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -4,12 +4,32 @@
 @section('content')
     <h1>Ini Halaman Student</h1>
     <h3>Student List</h3>
-    <ol>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>NIS</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($studentList as $data)
+            <tr>
+                <td>{{$loop->iteration}}</td>
+                <td>{{$data->name}}</td>
+                <td>{{$data->gender}}</td>
+                <td>{{$data->nis}}</td>
+            </tr>   
+            @endforeach
+        </tbody>
+    </table>
+    {{-- <ol>
         @foreach ($studentList as $data)
         <li>
             {{$data->name}} | {{$data->gender}} | {{$data->nis}}
         </li>   
         @endforeach
-    </ol>
+    </ol> --}}
 @endsection
 
