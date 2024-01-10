4625a202564d68281b92f34038128b33295f60de
Muhamad Royhan Fadhli <122357726+MRoyhanF@users.noreply.github.com>
2024-01-10 17:04:45 +0700

    betwen query builder & eloquent

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,8 +10,8 @@ class StudentController extends Controller
 {
     public function index()
     {
-        // $student = Student::all();
-        // return view('student', ['studentList' => $student]);
+        $student = Student::all();
+        return view('student', ['studentList' => $student]);
 
         // // query builder
         // $student = DB::table('students')->get();
@@ -22,14 +22,26 @@ class StudentController extends Controller
         //     'class_id' => 1
         // ]);
 
+        // DB::table('students')->where('id', 26)->update([
+        //     'name' => 'query builder 2',
+        //     'class_id' => 3
+        // ]);
+
+        // DB::table('students')->where('id', 26)->delete();
+
         // // eloquent
         // $student = Student ::all();
-        Student::create([
-            'name' => 'eloquent',
-                'gender' => 'P',
-                'nis' => '3274683',
-                'class_id' => 2
-        ]);
+        // Student::create([
+        //     'name' => 'eloquent',
+        //         'gender' => 'P',
+        //         'nis' => '3274683',
+        //         'class_id' => 2
+        // ]);
+        // Student::find(27)->update([
+        //     'name' => 'eloquery 2',
+        //     'class_id' => 1
+        // ]);
+        // Student::find(27)->delete();
 
         // dd($student);
     }
